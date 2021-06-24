fbed77a83d68fe22e88ea494ddaf34d462c38a61
corispade <cjspade44@gmail.com>
2021-06-23 22:05:53 -0400

    Adding the completed election analysis.

--- PyPoll.py
+++ PyPoll.py
@@ -80,6 +80,13 @@ with open(file_to_save, "w") as txt_file:
     # 5. Print out each candidates name, vote count and percentage of votes to terminal
     #print(f"{candidate_name}: {vote_percentage:.1f}% ({votes:,})\n")
 
+    #Add each candidate election results to the text file
+    candidate_results = (f"{candidate_name}: {vote_percentage:.1f}% ({votes:,})\n")
+    #Print the candidate, vote percentage and vote count to the terminal
+    print(candidate_results)
+    #Save candidate results to the text file
+    txt_file.write(candidate_results)
+
     #Determine winning vote count and candidate
     #Determine if the votes is greater than the winning count
     if (votes > winning_count) and (vote_percentage > winning_percentage):
@@ -93,14 +100,15 @@ with open(file_to_save, "w") as txt_file:
 
   #Print out the winning candidate summary
   winning_candidate_summary = (
-  f"-----------------------------\n"
-  f"Winner: {winning_candidate}\n"
-  f"Winning Vote Count: {winning_count:,}\n"
-  f"Winning Percentage: {winning_percentage:.1f}%\n"
-  f"-----------------------------\n"
-  )
-  #print(winning_candidate_summary)
-  # Diane DeGette was the winner of the election with 73.8% of the vote and 272,892 votes
+    f"-----------------------------\n"
+    f"Winner: {winning_candidate}\n"
+    f"Winning Vote Count: {winning_count:,}\n"
+    f"Winning Percentage: {winning_percentage:.1f}%\n"
+    f"-----------------------------\n"
+    )
+  print(winning_candidate_summary)
+  # Save candidate results to the text file
+  txt_file.write(winning_candidate_summary)
 
 
 
